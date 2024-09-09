b3760290cfc1058521a5ad8485592bdde0ba7789
compitcomds <compitcomds@gmail.com>
2024-09-09 16:05:01 +0530

    error in login page , vikas solve the error

--- pages/Auth/login.vue
+++ pages/Auth/login.vue
@@ -1,5 +1,35 @@
 <template>
     <div>
         Login
+        <input name="email" id="email" class="form-control" placeholder="Email Address" type="email"
+        v-model="formData.email" required />
+        <input name="password" id="password" placeholder="Password"
+                      type="password" v-model="formData.password" required />
+                    
+
     </div>
 </template>
+
+<script setup>
+
+import { loginUser } from "/appwrite/auth";
+
+// import loginUser
+
+const router = useRouter();
+
+const formData = useState("login-form", () => ({
+  email: "",
+  password: "",
+}));
+
+const submitForm = async () => {
+  try {
+    await loginUser(formData.value);
+    await router.replace("/dashboard/profile");
+  } catch (error) {
+    alert(`${error.message}`);
+  }
+};
+
+</script>
