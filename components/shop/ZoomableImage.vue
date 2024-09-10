6eb98ebd070c0693837ce6798b71fad1fdb74fca
Vikas-Maur <68019002+Vikas-Maur@users.noreply.github.com>
2024-09-11 01:35:11 +0530

    added pdp page

---
+++ components/shop/ZoomableImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :id="id">
+    <img
+      :src="image?.url || `https://placehold.co/500x500/png?text=${title}`"
+      :alt="image?.altText || `Image | ${title}`"
+      class="w-full object-cover sticky top-0"
+    />
+  </div>
+</template>
+
+<script setup>
+import ImageZoom from "js-image-zoom";
+const { image, title, id } = defineProps(["image", "id", "title"]);
+
+var options2 = {
+  fillContainer: true,
+  offset: { vertical: 0, horizontal: 10 },
+};
+
+onMounted(() => {
+  new ImageZoom(document.getElementById(id), options2);
+});
+</script>
+
+<style lang="scss" scoped></style>
