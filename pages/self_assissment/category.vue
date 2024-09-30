b08553b797ee752fdd59f9b065f9a651aecfbb2f
compitcomds <compitcomds@gmail.com>
2024-09-30 17:36:56 +0530

    rreg

---
+++ pages/self_assissment/category.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="w-full mx-auto px-10 flex flex-col lg:py-10 mb-16 lg:mb-0">
+        <div class=" p-6 rounded-lg text-center">
+            <h1 class="text-3xl font-semibold text-black">
+                Hi <span class="text-[#28574e] font-bold">Patient Name</span>, choose the skin condition of yours...
+            </h1>
+        </div>
+
+        <div class="flex flex-wrap items-center justify-center w-full">
+            <div class="flex-shrink-0 m-6 relative overflow-hidden bg-teal-500 rounded-lg w-96 h-96 shadow-lg group">
+                <svg class="absolute bottom-0 left-0 mb-8 scale-150 group-hover:scale-[1.65] transition-transform"
+                    viewBox="0 0 375 283" fill="none" style="opacity: 0.1;">
+                    <rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)"
+                        fill="white" />
+                    <rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="white" />
+                </svg>
+                <div
+                    class="relative pt-10 px-10 flex items-center justify-center group-hover:scale-110 transition-transform">
+                    <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                        style="background: radial-gradient(black, transparent 60%); transform: rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1); opacity: 0.2;">
+                    </div>
+                    <nuxt-link to="/self_assissment/category_type"><img class="relative" src="https://placehold.co/250x250" alt=""></nuxt-link>
+                </div>
+                <div class="relative flex justify-between text-white px-6 pb-6 mt-6">
+                    <div class="flex flex-col justify-between">
+                        <span class="block opacity-75 -mb-1">Condition</span>
+                        <span class="block font-semibold text-xl">Dry / Irritated Skin</span>
+
+                    </div>
+                    <span
+                        class="block bg-white rounded-full text-teal-500 text-xs font-bold px-8 leading-none flex items-center"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-move-right">
+                            <path d="M18 8L22 12L18 16" />
+                            <path d="M2 12H22" />
+                        </svg></span>
+                </div>
+            </div>
+            <div class="flex-shrink-0 m-6 relative overflow-hidden bg-teal-500 rounded-lg w-96 h-96 shadow-lg group">
+                <svg class="absolute bottom-0 left-0 mb-8 scale-150 group-hover:scale-[1.65] transition-transform"
+                    viewBox="0 0 375 283" fill="none" style="opacity: 0.1;">
+                    <rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)"
+                        fill="white" />
+                    <rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="white" />
+                </svg>
+                <div
+                    class="relative pt-10 px-10 flex items-center justify-center group-hover:scale-110 transition-transform">
+                    <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                        style="background: radial-gradient(black, transparent 60%); transform: rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1); opacity: 0.2;">
+                    </div>
+                    <nuxt-link><img class="relative" src="https://placehold.co/250x250" alt=""></nuxt-link>
+                </div>
+                <div class="relative flex justify-between text-white px-6 pb-6 mt-6">
+                    <div class="flex flex-col justify-between">
+                        <span class="block opacity-75 -mb-1">Condition</span>
+                        <span class="block font-semibold text-xl">Oily Skin</span>
+
+                    </div>
+                    <span
+                        class="block bg-white rounded-full text-teal-500 text-xs font-bold px-8 leading-none flex items-center"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-move-right">
+                            <path d="M18 8L22 12L18 16" />
+                            <path d="M2 12H22" />
+                        </svg></span>
+                </div>
+            </div>
+
+            <div class="flex-shrink-0 m-6 relative overflow-hidden bg-teal-500 rounded-lg w-96 h-96 shadow-lg group">
+                <svg class="absolute bottom-0 left-0 mb-8 scale-150 group-hover:scale-[1.65] transition-transform"
+                    viewBox="0 0 375 283" fill="none" style="opacity: 0.1;">
+                    <rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)"
+                        fill="white" />
+                    <rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="white" />
+                </svg>
+                <div
+                    class="relative pt-10 px-10 flex items-center justify-center group-hover:scale-110 transition-transform">
+                    <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                        style="background: radial-gradient(black, transparent 60%); transform: rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1); opacity: 0.2;">
+                    </div>
+                    <nuxt-link><img class="relative" src="https://placehold.co/250x250" alt=""></nuxt-link>
+                </div>
+                <div class="relative flex justify-between text-white px-6 pb-6 mt-6">
+                    <div class="flex flex-col justify-between">
+                        <span class="block opacity-75 -mb-1">Condition</span>
+                        <span class="block font-semibold text-xl">Normal Skin</span>
+
+                    </div>
+                    <span
+                        class="block bg-white rounded-full text-teal-500 text-xs font-bold px-8 leading-none flex items-center"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-move-right">
+                            <path d="M18 8L22 12L18 16" />
+                            <path d="M2 12H22" />
+                        </svg></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
--- pages/self_assissment/index.vue
+++ pages/self_assissment/index.vue
@@ -17,8 +17,8 @@
                 class="mt-1 p-3 w-full text-[#28574e] bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#28574e] focus:border-transparent transition duration-300"
               >
             </div>
-            <div class="w-full flex space-x-5">
-            <div class="mb-4 w-full">
+            
+            <div class="mb-4">
               <label for="sex" class="block text-sm font-medium text-gray-700">Sex</label>
               <select 
                 id="sex" 
@@ -32,7 +32,7 @@
               </select>
             </div>
   
-            <div class="mb-4 w-full">
+            <div class="mb-4">
               <label for="age" class="block text-sm font-medium text-gray-700">Age</label>
               <input 
                 type="number" 
@@ -41,10 +41,10 @@
                 required 
                 class="mt-1 p-3 w-full text-[#28574e] bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#28574e] focus:border-transparent transition duration-300"
               >
-            </div></div>
-  <div class="w-full flex space-x-5">
+            </div>
+  
             <!-- Conditional Pregnancy Status Field -->
-            <div v-if="showConditionalFields" class="mb-4 w-full">
+            <div v-if="showConditionalFields" class="mb-4">
               <label for="pregnancyStatus" class="block text-sm font-medium text-gray-700">Pregnancy Status</label>
               <select 
                 id="pregnancyStatus" 
@@ -59,7 +59,7 @@
             </div>
   
             <!-- Conditional Breastfeeding Field -->
-            <div v-if="showConditionalFields" class="mb-4 w-full">
+            <div v-if="showConditionalFields" class="mb-4">
               <label for="breastfeeding" class="block text-sm font-medium text-gray-700">Breastfeeding</label>
               <select 
                 id="breastfeeding" 
@@ -72,20 +72,20 @@
                 <option value="no">No</option>
               </select>
             </div>
-        </div>
-            <button 
-              type="submit" 
+  
+            <nuxt-link to="/self_assissment/concern_about"
+               
               class="w-full text-right flex items-center space-x-2 justify-end text-[#28574e] py-3 rounded-md transition duration-300 font-semibold"
             >
               <p class="text-lg">Next Page</p>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-            </button>
+            </nuxt-link>
           </form>
         </div>
         
         <!-- Illustration Section -->
         <div class="hidden lg:flex items-center justify-center w-1/2">
-          <img src="https://placehold.co/600x300" alt="Form Illustration" class="w-full h-[500px]">
+          <img src="https://placehold.co/600x400" alt="Form Illustration" class="w-full h-[500px]">
         </div>
       </div>
     </div>
