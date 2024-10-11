9d5cb050263dc4f694405ffa8a0e635f55452840
compitcomds <compitcomds@gmail.com>
2024-10-11 12:28:14 +0530

    updates by dipesh

--- components/home/block.vue
+++ components/home/block.vue
@@ -1,19 +1,3 @@
-<!-- <template>
-  <nuxt-link
-    to="#"
-    class="flex items-center justify-between mt-0 lg:mt-72 object-cover"
-  >
-    <img
-      src="https://ccdstest.b-cdn.net/Medi%20u/Untitled%20design%20(11)%20(1).png"
-      alt=""
-  /></nuxt-link>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped></style> -->
 <template>
   <div class="flex items-center justify-between mt-0 lg:mt-72 object-cover bg-white w-full">
     <swiper
