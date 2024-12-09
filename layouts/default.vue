2b642dcfe758b064293c179d98e6aab9dd524cb5
Vikas Maur <vikasmaur10@gmail.com>
2024-12-09 18:51:04 +0530

    removed auth required page...

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen flex flex-col justify-between bg-gray-50 text-gray-800"
+    class="flex min-h-screen flex-col justify-between bg-gray-50 text-gray-800"
   >
     <header>
       <HomeNavbar />
@@ -8,64 +8,11 @@
 
     <main class="flex-grow">
       <div v-if="!isLoading">
-        <div v-if="!preventAccess">
-          <slot />
-        </div>
-        <div
-          v-else
-          class="my-20 max-w-lg mx-auto bg-blue-50 rounded-lg shadow-md p-8 text-center space-y-6"
-        >
-          <!-- Movable Icon with Hover Effect -->
-          <div
-            class="flex justify-center mb-4 transform transition-transform duration-300 hover:translate-y-1"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-12 h-12 text-blue-600"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-            </svg>
-          </div>
-
-          <!-- Header Text -->
-          <h2 class="text-2xl font-semibold text-blue-800">
-            Authentication Required
-          </h2>
-
-          <!-- Instructions Text -->
-          <p class="text-md text-gray-700">
-            To access this content, please
-            <nuxt-link
-              to="/auth/login"
-              class="text-blue-700 font-medium hover:underline focus:outline-none"
-            >
-              Login
-            </nuxt-link>
-            or
-            <nuxt-link
-              to="/auth/register"
-              class="text-blue-700 font-medium hover:underline focus:outline-none"
-            >
-              Sign Up </nuxt-link
-            >.
-          </p>
-
-          <!-- Additional Information -->
-          <p class="text-sm text-gray-500">
-            If you encounter any issues, our support team is here to help. Reach
-            out for assistance anytime.
-          </p>
-        </div>
+        <slot />
       </div>
 
-      <div v-else class="min-h-[50vh] flex items-center justify-center">
-        <div class="flex flex-col justify-center items-center gap-5">
+      <div v-else class="flex min-h-[50vh] items-center justify-center">
+        <div class="flex flex-col items-center justify-center gap-5">
           <div class="animate-spin text-[#238878]">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -82,21 +29,17 @@
               <path d="M21 12a9 9 0 1 1-6.219-8.56" />
             </svg>
           </div>
-          <h1 class="text-center font-bold text-4xl">Loading...</h1>
+          <h1 class="text-center text-4xl font-bold">Loading...</h1>
         </div>
       </div>
     </main>
 
-    <!-- <footer>
-      <SiteFooter class="hidden lg:block bg-gray-800 text-white py-6" />
-    </footer> -->
-
-    <div class="lg:hidden fixed bottom-0 w-full z-10">
+    <div class="fixed bottom-0 z-10 w-full lg:hidden">
       <MobileBottomNav />
     </div>
   </div>
 
-  <SiteFooter class="hidden lg:block " />
+  <SiteFooter class="hidden lg:block" />
 
   <MobileBottomNav />
 </template>
@@ -105,22 +48,19 @@
 import { getUser } from "~/appwrite/auth";
 
 const route = useRoute();
-
-const preventAccess = ref(false);
+const router = useRouter();
 const isLoading = ref(false);
 
 const LOGIN_REQUIRED_ROUTES = ["/checkout", "/dashboard", "/consultancy"];
 
 const blockUnauthenticatedUser = async (pathname: string) => {
-  preventAccess.value = false;
   isLoading.value = true;
   for (const loginRequiredRoute of LOGIN_REQUIRED_ROUTES) {
-    if (pathname.includes(loginRequiredRoute)) {
-      try {
-        const user = await getUser();
-      } catch (error) {
-        preventAccess.value = true;
-      }
+    if (!pathname.includes(loginRequiredRoute)) continue;
+    try {
+      await getUser();
+    } catch (error) {
+      router.replace("/auth/login");
       break;
     }
   }
