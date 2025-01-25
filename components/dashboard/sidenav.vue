695f5dcfa80bff255707abf9c8a0b79863583f53
Vikas Maur <vikasmaur10@gmail.com>
2025-01-25 17:43:07 +0530

    removed sidenav css

--- components/dashboard/sidenav.vue
+++ components/dashboard/sidenav.vue
@@ -1,7 +1,11 @@
 <template>
-  <aside class="w-full rounded-lg bg-white p-6 pb-20 lg:w-1/4">
+  <aside class="w-full rounded-lg bg-[#f9fafb] p-6 pb-20 lg:w-1/4">
     <!-- Header Title -->
-    <h2 class="mb-6 text-lg font-semibold text-gray-800">Your Account</h2>
+    <h2
+      class="mb-6 text-[1.125rem] text-lg font-semibold text-[238878] text-gray-800"
+    >
+      Your Account
+    </h2>
 
     <!-- Profile Section -->
     <div class="mb-6 flex flex-col items-center rounded-lg bg-gray-100 p-6">
@@ -13,7 +17,11 @@
         />
       </div>
       <div class="mt-4 text-center">
-        <h2 class="text-lg font-semibold text-gray-800">{{ user.name }}</h2>
+        <h2
+          class="text-[1.125rem] text-lg font-semibold text-[238878] text-gray-800"
+        >
+          {{ user.name }}
+        </h2>
         <p class="text-sm text-gray-600">{{ user.email }}</p>
         <p class="text-sm text-gray-600">{{ user.phone }}</p>
       </div>
@@ -23,7 +31,7 @@
       <ul class="space-y-4">
         <li
           v-for="tab in tabs"
-          class="flex items-center justify-between rounded-md hover:bg-gray-100"
+          class="flex items-center justify-between rounded-md text-[1rem] font-medium hover:bg-gray-100"
         >
           <nuxt-link :to="tab.href" class="flex w-full items-center gap-2 p-3">
             <component :is="tab.image" class="tab-icon h-6 w-6" />
@@ -106,27 +114,4 @@ const tabs = [
 ];
 </script>
 
-<style scoped>
-/* Adjust colors, fonts, and spacing */
-aside {
-  background-color: #f9fafb;
-}
-
-h2 {
-  font-size: 1.125rem;
-  color: #238878;
-}
-
-nav ul li {
-  font-size: 1rem;
-  font-weight: 500;
-}
-
-.text-blue-500 {
-  color: #5885ff;
-}
-
-.text-black {
-  color: #000;
-}
-</style>
+<style scoped></style>
